28e2f5f4b88bb4a935c47d709073625400a25544
Sonika-kendra <s.parmar@kendra-intl.com>
2026-05-07 18:30:34 +0530

    Update metal type to black color

--- src/data/shop/metalTypes.ts
+++ src/data/shop/metalTypes.ts
@@ -14,24 +14,24 @@ export interface MetalType {
 
 export const metalTypes: MetalType[] = [
   // Yellow Gold
-  { id: '22K_YG', label: '22K', name: '22ct Yellow Gold', group: 'yellow-gold', bg: '#C9922A', color: '#fff', image: yellowGoldImg },
-  { id: '18K_YG', label: '18K', name: '18ct Yellow Gold', group: 'yellow-gold', bg: '#C5A028', color: '#fff', image: yellowGoldImg },
-  { id: '9K_YG',  label: '9K',  name: '9ct Yellow Gold',  group: 'yellow-gold', bg: '#C5A028', color: '#fff', image: yellowGoldImg },
+  { id: '22K_YG', label: '22K', name: '22ct Yellow Gold', group: 'yellow-gold', bg: '#C9922A', color: '#000', image: yellowGoldImg },
+  { id: '18K_YG', label: '18K', name: '18ct Yellow Gold', group: 'yellow-gold', bg: '#C5A028', color: '#000', image: yellowGoldImg },
+  { id: '9K_YG',  label: '9K',  name: '9ct Yellow Gold',  group: 'yellow-gold', bg: '#C5A028', color: '#000', image: yellowGoldImg },
 
   // White Gold
-  { id: '18K_WG', label: '18K', name: '18ct White Gold', group: 'white-gold', bg: '#B8B8C8', color: '#333', image: whiteGoldImg },
-  { id: '9K_WG',  label: '9K',  name: '9ct White Gold',  group: 'white-gold', bg: '#B8B8C8', color: '#333', image: whiteGoldImg },
+  { id: '18K_WG', label: '18K', name: '18ct White Gold', group: 'white-gold', bg: '#B8B8C8', color: '#000', image: whiteGoldImg },
+  { id: '9K_WG',  label: '9K',  name: '9ct White Gold',  group: 'white-gold', bg: '#B8B8C8', color: '#000', image: whiteGoldImg },
 
   // Rose Gold
-  { id: '18K_RG', label: '18K', name: '18ct Rose Gold', group: 'rose-gold', bg: '#C08070', color: '#fff' },
-  { id: '9K_RG',  label: '9K',  name: '9ct Rose Gold',  group: 'rose-gold', bg: '#C08070', color: '#fff' },
+  { id: '18K_RG', label: '18K', name: '18ct Rose Gold', group: 'rose-gold', bg: '#C08070', color: '#000' },
+  { id: '9K_RG',  label: '9K',  name: '9ct Rose Gold',  group: 'rose-gold', bg: '#C08070', color: '#000' },
 
   // Platinum
-  { id: 'Pt950', label: 'Pt', name: 'Platinum 950', group: 'platinum', bg: '#7A7A8E', color: '#fff', image: platinumImg },
-  { id: 'Pt900', label: 'Pt', name: 'Platinum 900', group: 'platinum', bg: '#7A7A8E', color: '#fff', image: platinumImg },
+  { id: 'Pt950', label: 'Pt', name: 'Platinum 950', group: 'platinum', bg: '#7A7A8E', color: '#000', image: platinumImg },
+  { id: 'Pt900', label: 'Pt', name: 'Platinum 900', group: 'platinum', bg: '#7A7A8E', color: '#000', image: platinumImg },
 
   // Silver
-  { id: 'Ag925', label: 'Ag', name: 'Sterling Silver', group: 'silver', bg: '#A0A0A0', color: '#fff' },
+  { id: 'Ag925', label: 'Ag', name: 'Sterling Silver', group: 'silver', bg: '#A0A0A0', color: '#000' },
 ];
 
 export const metalTypeMap = Object.fromEntries(metalTypes.map((m) => [m.id, m])) as Record<string, MetalType>;
@@ -45,7 +45,7 @@ export function getMetalType(id: string): MetalType {
       name: id,
       group: 'yellow-gold',
       bg: '#C5A028',
-      color: '#fff',
+      color: '#000',
     }
   );
 }
